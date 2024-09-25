e06d847284f6a15f1da77ee8f130760bc2078eae
ManishPrajapati273 <manishprajapati2739@gmail.com>
2024-09-25 18:27:10 +0530

    Update

---
+++ script.js
@@ -0,0 +1,8 @@
+let main = document.getElementsByClassName("main");
+
+let logbtn = document.getElementById("logbtn");
+
+logbtn.addEventListener("click", function() {
+  
+ window.location.assign("login.html"); })
+ 
